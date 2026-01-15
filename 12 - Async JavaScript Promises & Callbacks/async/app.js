8008fc4e36272c71f623151b4024947f5b973785
hishamrehab <hishamrehab30@gmail.com>
2026-01-16 00:42:34 +0200

    Promises

--- 12 - Async JavaScript Promises & Callbacks/async/app.js
+++ 12 - Async JavaScript Promises & Callbacks/async/app.js
@@ -1,17 +1,31 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const setTimer = (duration) => {
+  const promise = new Promise((resolve , reject) => {
+  setTimeout( () => {
+    resolve("Done!");
+} , duration);   
+  });
+
+return promise;
+}
+
+
+
 function trackUserHandler() {
-   navigator.geolocation.getCurrentPosition(posData => {
-   setTimeout(()  => {
-     console.log(posData);
-   }, 2000);
-   }, error => {
+   navigator.geolocation.getCurrentPosition(
+    posData => {
+    setTimer(2000).then(data => {
+      console.log(data , posData);
+    });
+  },
+     error => {
     console.log(error);
    });
-   setTimeout() => {
+   setTimeout(() => {
     console.log('Getting position...');
-   }
+   }, 2000)
    console.log('Getting position...');
 }
 
