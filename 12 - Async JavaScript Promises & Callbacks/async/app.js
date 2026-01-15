b7287e53b27cd5d59ed776dd959eaef38ecf4805
hishamrehab <hishamrehab30@gmail.com>
2026-01-16 00:24:23 +0200

    event loop and multiple callbacks

--- 12 - Async JavaScript Promises & Callbacks/async/app.js
+++ 12 - Async JavaScript Promises & Callbacks/async/app.js
@@ -2,7 +2,24 @@ const button = document.querySelector('button');
 const output = document.querySelector('p');
 
 function trackUserHandler() {
-  console.log('Clicked!');
+   navigator.geolocation.getCurrentPosition(posData => {
+   setTimeout(()  => {
+     console.log(posData);
+   }, 2000);
+   }, error => {
+    console.log(error);
+   });
+   setTimeout() => {
+    console.log('Getting position...');
+   }
+   console.log('Getting position...');
 }
 
 button.addEventListener('click', trackUserHandler);
+
+// let result = 0;
+// for (i = 0 ; i < 1000000; i++) {
+//   result += i;
+// }
+
+// console.log(result);
