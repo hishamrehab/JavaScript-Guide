dda424124d80db59ca949e504fc1cbed7f892ce2
hishamrehab <hishamrehab30@gmail.com>
2026-01-31 10:48:27 +0200

    Inheritance using Classes

---
+++ 13- Inheritance - Prototypal Inheritance/index.js
@@ -0,0 +1,38 @@
+// Inheritance using Classes
+class Person {
+    talk() {
+        return "Taking"
+    }
+}
+
+const me = new Person();
+const you = new Person();
+
+console.log(me.talk());  // Talking
+console.log(you.talk()); // Talking
+
+
+
+// To update the function for both instances you only have to do it once:
+Person.prototype.talk = function () {
+    return "New and improved Talking";
+}
+
+
+// Inheritance using a Constructor Function
+function Person() { };
+Person.prototype.talk = function () {
+    return "Talking";
+}
+
+const me = new Person();
+const you = new Person();
+
+me.talk(); // Talking
+you.talk(); // Talking
+
+
+
+// -----------------------------------------
+// Inheritance using pure objects with Object.create
+
