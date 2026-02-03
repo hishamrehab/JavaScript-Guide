70c415a485b8658e7ecc54a76acde66c69aa2c99
hishamrehab <hishamrehab30@gmail.com>
2026-02-03 12:57:40 +0200

    Value vs. Reference

---
+++ 21- Value vs. Reference/index.js
@@ -0,0 +1,76 @@
+const { use } = require("react");
+
+const a = 1;
+
+const b = 2;
+
+a === b; // true 
+[a] = 1;
+[b] = 2;
+
+[a] === [b]; // false
+
+
+const myName = "hisham"
+const a = 1;
+const b = 1
+
+
+
+
+
+const a = { skills: "Cooking" }
+const b = { skills: "Cooking" }
+
+a === b; // false
+
+
+
+
+function incremantAge(age) {
+    age = age + 1
+}
+
+const myAge = 10;
+incremantAge(myAge);
+console.log(myAge); // 10
+
+
+
+function incremantAge(user) {
+    user.age = user.age + 1
+}
+
+const me = { age: 10 }
+
+incremantAge(me);
+console.log(me.age); // 11
+
+
+
+
+let a = 1;
+let b = a;
+
+
+a //1 
+b// 2
+a = 7;
+b = 1
+
+
+
+a = {
+    name: "Hello"
+}
+
+b = {
+    n
+}
+
+
+
+
+
+
+
